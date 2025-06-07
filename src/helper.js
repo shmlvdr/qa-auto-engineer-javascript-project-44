652202a2bc2825662e2c2a05b7dd051177731658
Daria <root@DESKTOP-U4P88TD.localdomain>
2025-06-07 20:32:57 +0400

    Change for check #0

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,1 @@
-import { randomBytes } from 'crypto';
-
-export const getRandomNumber = (min, max) => {
-  const randomValue = randomBytes(1)[0];
-  return Math.floor(randomValue / 256 * (max - min + 1)) + min;
-};
+export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
