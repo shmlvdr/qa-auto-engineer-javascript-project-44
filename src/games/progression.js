7ca7aaeb483927e50055b5cb50089c8e6383c33c
Daria <root@DESKTOP-U4P88TD.localdomain>
2025-06-07 19:03:24 +0400

    Step 8

---
+++ src/games/progression.js
@@ -0,0 +1,26 @@
+import runGame from '../index.js';
+import { getRandomNumber } from '../helper.js';
+
+const description = 'What number is missing in the progression?';
+
+const getQuestionAndAnswer = () => {
+    const progressionLength = getRandomNumber(5, 10);
+    const start = getRandomNumber(1, 50);
+    const step = getRandomNumber(1, 10);
+
+    const progression = [];
+    for (let i = 0; i < progressionLength; i++) {
+        progression.push(start + i * step);
+    }
+
+    const hiddenElementIndex = getRandomNumber(0, progressionLength - 1);
+    const correctAnswer = progression[hiddenElementIndex];
+    
+    progression[hiddenElementIndex] = '..';
+
+    const question = progression.join(' ');
+
+    return [question, String(correctAnswer)];
+};
+
+export default () => runGame(description, getQuestionAndAnswer);
